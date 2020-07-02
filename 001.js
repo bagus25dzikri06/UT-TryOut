a8b2af42ea6c8807689ea2e2e20f8adb98f21191
bagus25dzikri06 <bagusdzikri@gmail.com>
2020-07-02 16:08:00 +0700

    Jawaban soal no. 1

---
+++ 001.js
@@ -0,0 +1,13 @@
+function grade(nilai) {
+	if (nilai >= 90) {
+		console.log('Nilai Anda A')
+	} else if ((nilai >= 80) && (nilai < 90)) {
+		console.log('Nilai Anda B')
+	} else if ((nilai >= 70) && (nilai < 80)) {
+		console.log('Nilai Anda C')
+	} else if ((nilai >= 60) && (nilai < 70)) {
+		console.log('Nilai Anda D')
+	} else {
+		console.log('Nilai Anda E')
+	}
+}	
