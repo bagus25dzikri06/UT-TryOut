649b3f3344d849024224c02c226cc50345515a9a
bagus25dzikri06 <bagusdzikri@gmail.com>
2020-07-02 15:35:25 +0700

    Jawaban soal no. 3

--- 003.js
+++ 003.js
@@ -1,14 +1,18 @@
 function max(array) {
 	if (array.length < 100) {
+		//Mengurutkan bilangan
 		array.sort(function(a,b) {return a-b})
 	}
+	//Mengambil nilai tertinggi
 	console.log('Nilai maksimum: ' + array[array.length - 1])
 }
 
 function min(array) {
 	if (array.length < 100) {
+		//Mengurutkan bilangan
 		array.sort(function(a,b) {return b-a})
 	}
+	//Mengambil nilai terendah
 	console.log('Nilai minimum: ' + array[array.length - 1])
 }
 
@@ -16,11 +20,13 @@ function average(array) {
 	var jumlah = 0, rataRata
 	
 	if (array.length < 100) {
+		//Menjumlahkan nilai dari suatu array
 		for (var i = 0; i < array.length; i++) {
 			jumlah += array[i]
 		}
 	}
 	
+	//Nilai rata-rata dari suatu array 
 	rataRata = jumlah / array.length
 	console.log('Nilai rata-rata: ' + rataRata)
 }
